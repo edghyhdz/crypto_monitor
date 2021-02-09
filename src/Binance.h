7078ab82b17fec4d3ea7795b04225e65f01aabcc
edghyhdz <edgar.hdz@hotmail.com>
2021-02-09 06:03:24 +0100

    Added references, and callback method for CURL requests

--- src/Binance.h
+++ src/Binance.h
@@ -8,30 +8,37 @@
 namespace Binance {
 // Paths
 const std::string BASE_URL{"https://api.binance.com/api/v3/aggTrades?limit=775&symbol="};
-const std::string USED_WEIGHT{"x-mbx-used-weight:"};
-const std::string USED_WEIGHT_PER_INTERVAL{"x-mbx-used-weight-1m:"};
+const std::string USED_WEIGHT{"x-mbx-used-weight"};
+const std::string USED_WEIGHT_PER_INTERVAL{"x-mbx-used-weight-1m"};
 const std::string HTTP_RESPONSE{"HTTP/1.1 200 OK"};
 const std::string RESPONSE{"Response"};
 const std::string OK_RESPONSE{"200"};
-const int KEY_COUNTER(3); 
+const int KEY_COUNTER(3);
 
-// Split 
-std::vector<std::string> split(const std::string& str, const std::string& delim)
-{
-    std::vector<std::string> tokens;
-    size_t prev = 0, pos = 0;
-    do
-    {
-        pos = str.find(delim, prev);
-        if (pos == std::string::npos) pos = str.length();
-        std::string token = str.substr(prev, pos-prev);
-        if (!token.empty()) tokens.push_back(token);
-        prev = pos + delim.length();
-    }
-    while (pos < str.length() && prev < str.length());
-    return tokens;
+// Split
+// reference https://stackoverflow.com/a/14266139/13743493
+std::vector<std::string> split(const std::string &str,
+                               const std::string &delim) {
+  std::vector<std::string> tokens;
+  size_t prev = 0, pos = 0;
+  do {
+    pos = str.find(delim, prev);
+    if (pos == std::string::npos)
+      pos = str.length();
+    std::string token = str.substr(prev, pos - prev);
+    if (!token.empty())
+      tokens.push_back(token);
+    prev = pos + delim.length();
+  } while (pos < str.length() && prev < str.length());
+  return tokens;
 }
 
+// Callback to curl function
+// Reference https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
 
 } // namespace Binance
 
