70a16f58398c5415a4c65791d64175af196ce023
edghyhdz <edgar.hdz@hotmail.com>
2021-02-10 21:35:39 +0100

    src/QueryCrypto.cpp
    Added first part of saving and dealing with data from second end point to binance

--- src/QueryCrypto.h
+++ src/QueryCrypto.h
@@ -11,17 +11,20 @@ Crypto currency query class declaration
 class QueryCrypto {
 public:
   // Currency pair
-  QueryCrypto(std::string curPair, int id) : _curPair(curPair), _id(id) {};
+  QueryCrypto(std::string curPair, int id, bool allCoins) : _curPair(curPair), _id(id), _allCoins(allCoins) {};
 
   // Making http request
   void getData(); 
-  void runQueries(); 
+  void getAllData(); 
+  void runSingleQueries(); 
   // Data processing
   void saveCSVData(std::string &readBuffer); 
+  void saveAllCoinsCSVData(std::string &readBuffer); 
   std::map<std::string, std::string> parseHeaderData(std::string &readBuffer); 
 
   // getters
   std::string getCoinPair(){ return _curPair; }
+  bool allQueries(){ return _allCoins; }
 
   void printCoinPair() { std::cout << "Coin: " << _curPair << std::endl; }; 
 
@@ -29,6 +32,7 @@ private:
   std::string _curPair;                 // crypto currency pair i.e., BTC/USD
   std::string _currentWeight;           // Get info about current interval weight
   int _id; 
+  bool _allCoins; 
 
 };
 
