f857cf28e81b6f66f44a8c35b6389b485704b204
edghyhdz <edgar.hdz@hotmail.com>
2021-02-10 21:34:45 +0100

    Added private members to start thread for the querying of all coins on binance

--- src/Orchestrator.cpp
+++ src/Orchestrator.cpp
@@ -15,15 +15,21 @@ g++ -g src/QueryCrypto.cpp src/QueryCrypto.h src/Binance.h src/Orchestrator.cpp 
 
 Orchestrator::Orchestrator() {
   // TODO: Put this into a txt file
-  std::vector<std::string> currencies{"REEFUSDT", "CHZUSDT", "MATICUSDT", "VETUSDT", "FETUSDT", "ZILUSDT", "RLCUSDT", "OCEANUSDT", "ATOMUSDT", "LUNAUSDT"};
+  // std::vector<std::string> currencies{"REEFUSDT", "CHZUSDT", "MATICUSDT", "VETUSDT", "FETUSDT", "ZILUSDT", "RLCUSDT", "OCEANUSDT", "ATOMUSDT", "LUNAUSDT"};
+  std::vector<std::string> currencies{"BTCUSDT"};
+
   int id_currency = 0;
 
   for (std::string coin : currencies) {
       std::cout << "Coin: " << coin << std::endl; 
     std::shared_ptr<QueryCrypto> crypto =
-        std::make_shared<QueryCrypto>(coin, ++id_currency);
+        std::make_shared<QueryCrypto>(coin, ++id_currency, false);
     _cryptos.emplace_back(crypto);
   }
+
+  // For querying all data from all coins on binance
+  _allCryptos = std::make_shared<QueryCrypto>("all", ++id_currency, true); 
+
 }
 
 Orchestrator::~Orchestrator() {
@@ -42,8 +48,11 @@ Orchestrator::~Orchestrator() {
 
 void Orchestrator::runQuery() {
   for (auto &c : _cryptos) {
-    _threadCryptos.emplace_back(std::thread(&QueryCrypto::runQueries, c));
+    _threadCryptos.emplace_back(std::thread(&QueryCrypto::runSingleQueries, c));
   }
+
+  _threadCryptos.emplace_back(std::thread(&QueryCrypto::runSingleQueries, _allCryptos)); 
+
 }
 
 int main() {
