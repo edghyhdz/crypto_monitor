0ce63873acf573d421e49522256a7f8acc64402b
edghyhdz <edgar.hdz@hotmail.com>
2021-02-13 10:26:02 +0100

    Added private member attributes, as well as setters and getters to work with dashhboard data

--- src/QueryCrypto.h
+++ src/QueryCrypto.h
@@ -1,10 +1,12 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
-
+#define ALL_COINS_ID 205  // Generic all coins ID
+#define COIN_TO_PLOT "ENJUSDTT "
 #include <string>
 #include <iostream>
 #include <map>
 #include <vector>
+
 
 /*
 Crypto currency query class declaration
@@ -24,15 +26,21 @@ public:
   void saveData(std::vector<std::vector<std::string>> &chunkData); 
   std::map<std::string, std::string> parseHeaderData(std::string &readBuffer); 
 
+  // setters
+  void setCoinToPlot(std::string coinToPlot) { _cointToPlot = coinToPlot;}
+  void setPlotData(std::vector<std::vector<std::string>> &plotData) { _plotData = plotData; }
   // getters
+  int getID() { return _id; }
+  std::vector<std::vector<std::string>> getPlotData() { return _plotData; }
   std::string getCoinPair(){ return _curPair; }
+  std::string getCoinToPlot(){ return _cointToPlot; }
   bool allQueries(){ return _allCoins; }
 
-  void printCoinPair() { std::cout << "Coin: " << _curPair << std::endl; }; 
-
 private:
-  std::string _curPair;                 // crypto currency pair i.e., BTC/USD
-  std::string _currentWeight;           // Get info about current interval weight
+  std::string _curPair;                               // crypto currency pair i.e., BTC/USD
+  std::string _currentWeight;                         // Get info about current interval weight
+  std::vector<std::vector<std::string>> _plotData;    // Data to plot into dashboard
+  static std::string _cointToPlot;                    // Coin to be plotted
   int _id; 
   bool _allCoins; 
 
