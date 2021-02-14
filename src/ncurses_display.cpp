d73421d0b5001875bce6ab4983188f0ad96b01dc
edghyhdz <edgar.hdz@hotmail.com>
2021-02-14 12:54:32 +0100

    Added editViewWindow -> modified version from flarn2006's repository References https://github.com/flarn2006/MiscPrograms/blob/master/graph.c

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -16,6 +16,8 @@ Plot graph cool repo from flarn2006
 #include <fstream>
 #include <math.h>
 #include "ncurses_display.h"
+#include <form.h>
+#include <string.h>
 
 using namespace NCursesDisplay; 
 using std::string;
@@ -56,23 +58,53 @@ void NCursesDisplay::DrawAxes(WINDOW *window, const viewwin *view) {
   double xstep, ystep;
   getViewStep(window, view, &xstep, &ystep);
 
-  wattron(window, COLOR_PAIR(2));
   int i;
-  for (i = 2; i <= xm - 2; i++) {
-    double plotx = view->xmin + xstep * i;
-    float abs_val = fabs(fmod(plotx, view->xscl)); 
-    int tick = abs_val < xstep;
-    mvwaddch(window, ym - 2, i, tick ? ACS_PLUS : ACS_HLINE);
-  }
   for (i = 2; i <= ym - 2; i++) {
     double ploty = view->ymin + ystep * i;
     float abs_val = fabs(fmod(ploty, view->yscl));
     int tick = abs_val < xstep;
+
+    // Add grid in the x direction
+    if (tick == 1) {
+      wattron(window, COLOR_PAIR(9));
+      for (int k = 4; k <= xm - 2; k++) {
+        mvwaddch(window, i, k, ACS_HLINE);
+      }
+      wattroff(window, COLOR_PAIR(9));
+    }
+    wattron(window, COLOR_PAIR(2));
     mvwaddch(window, i, view->xmin + 2, tick ? ACS_PLUS : ACS_VLINE);
   }
+
+  for (i = 2; i <= xm - 2; i++) {
+    double plotx = view->xmin + xstep * i;
+    float abs_val = fabs(fmod(plotx, view->xscl)); 
+    int tick = abs_val < xstep;
+
+    // Add grid in the y direction
+    if (tick == 1) {
+      wattron(window, COLOR_PAIR(9));
+      for (int k = 4; k <= ym - 2; k++) {
+        double ploty = view->ymin + ystep * k;
+        float abs_val = fabs(fmod(ploty, view->yscl));
+        int tick = abs_val < xstep;
+
+        if (k == 5) {
+          mvwaddch(window, k, i, ACS_TTEE);
+        } else if (k > 5){
+          mvwaddch(window, k, i, tick ? ACS_PLUS : ACS_VLINE);
+        }
+      }
+      wattroff(window, COLOR_PAIR(9));
+    }
+
+    wattron(window, COLOR_PAIR(2));
+    mvwaddch(window, ym - 2, i, tick ? ACS_PLUS : ACS_HLINE);
+    wattroff(window, COLOR_PAIR(2));
+  }
+
   mvwaddch(window, ym - 4, view->xmin + 2, ACS_PLUS);
   wattroff(window, COLOR_PAIR(2));
-
 }
 
 void getMaxMinValue(std::vector<double> data, double *value_x, double *value_y, double pct){
@@ -86,6 +118,99 @@ void getMaxMinValue(std::vector<double> data, double *value_x, double *value_y, 
   *value_y = *value_y - *value_y * pct;
 }
 
+int NCursesDisplay::editViewWindow(viewwin *view)
+{
+	/* Figure out where to put the window */
+	int wwidth = 21, wheight = 11;
+	int ym, xm; getmaxyx(stdscr, ym, xm);
+	int wy = ym/2 - wheight/2;
+	int wx = xm/2 - wwidth/2;
+
+	/* Create a new window and draw the form */
+	WINDOW *fwin = newwin(wheight, wwidth, wy, wx);
+	keypad(fwin, TRUE);
+	werase(fwin);
+	
+	wattron(fwin, A_BOLD);
+	box(fwin, 0, 0);
+	mvwprintw(fwin, 0, 4, " VIEW WINDOW ");
+	mvwprintw(fwin, 2, 3, "COIN 1 =");
+	mvwprintw(fwin, 3, 3, "COIN 2 =");
+
+	wattroff(fwin, A_BOLD);
+	mvwprintw(fwin, 9, 6, "[SPC] OK");
+	/* Create the form fields */
+	FIELD *fields[3];
+	int i; for (i=0; i<2; i++) {
+		fields[i] = new_field(1, 8, i, 0, 0, 0);
+		set_field_back(fields[i], A_REVERSE | A_UNDERLINE);
+		set_field_type(fields[i], TYPE_ALNUM, 6, 0.0, 0.0);
+		// set_field_type(fields[i], TYPE_NUMERIC, 6, 0.0, 0.0);
+		field_opts_off(fields[i], O_AUTOSKIP | O_STATIC);
+		set_max_field(fields[i], FIELD_MAX_CHARS);
+	}
+	fields[2] = NULL;
+
+	/* Fill the form fields with initial values */
+	char printbuf[FIELD_MAX_CHARS+1];
+	set_field_buffer(fields[0], 0, (view->current_coin).c_str());
+
+	/* Create a subwindow for the form fields */
+	WINDOW *fsub = derwin(fwin, 6, 8, 2, 10);
+	keypad(fsub, TRUE);
+	
+	/* Create the actual form */
+	FORM *f = new_form(fields);
+	set_form_win(f, fwin);
+	set_form_sub(f, fsub);
+	post_form(f);
+	wrefresh(fwin);
+	wrefresh(fsub);
+	curs_set(1);
+
+	/* Handle input */
+	int savewin = 1;
+	int exitloop = 0;
+	int ch; while (!exitloop) {
+		switch (ch = wgetch(fwin)) {
+			case 'j': case '\n': case KEY_DOWN:
+				form_driver(f, REQ_NEXT_FIELD);
+				break;
+			case 'k': case KEY_UP:
+				form_driver(f, REQ_PREV_FIELD);
+				break;
+			case KEY_BACKSPACE:
+				form_driver(f, REQ_DEL_PREV);
+				break;
+			case ' ':
+				exitloop = 1;
+				break;
+			case 'q':
+				savewin = 0;
+				exitloop = 1;
+				break;
+			default:
+				form_driver(f, ch);
+				break;
+		}
+	}
+
+	if (savewin) {
+          form_driver(f, REQ_VALIDATION);
+          view->current_coin = field_buffer(fields[0], 0);
+	}
+
+  /* Clean up */
+	curs_set(0);
+	unpost_form(f);
+	free_form(f);
+	for (i=0; i<6; i++) free_field(fields[i]);
+	delwin(fsub);
+	delwin(fwin);
+	refresh();
+
+	return savewin;
+}
 
 void NCursesDisplay::DrawGraph(WINDOW *window, const viewwin *view, std::vector<std::vector<std::string>> &plotData) {
   /* Draws a graph on the screen without axes.
@@ -133,9 +258,12 @@ void NCursesDisplay::DrawGraph(WINDOW *window, const viewwin *view, std::vector<
 
   wattroff(window, COLOR_PAIR(4));
   wattron(window, COLOR_PAIR(4));
-  mvwprintw(window, 1, xm - 35, ("Max price     : " + to_string(max_price) + " USDT").c_str());
-  mvwprintw(window, 2, xm - 35, ("Min price     : " + to_string(min_price) + " USDT").c_str());
-  mvwprintw(window, 3, xm - 35, ("Current price : " + to_string(last_price) + " USDT").c_str());
+  wattron(window, A_BOLD);
+  mvwprintw(window, 1, 5, ("COIN: " + view->current_coin).c_str());
+  mvwprintw(window, 1, xm - 35, ("Current price : " + to_string(last_price) + " USDT").c_str());
+  wattroff(window, A_BOLD);
+  mvwprintw(window, 2, xm - 35, ("Max price     : " + to_string(max_price) + " USDT").c_str());
+  mvwprintw(window, 3, xm - 35, ("Min price     : " + to_string(min_price) + " USDT").c_str());
   wattroff(window, COLOR_PAIR(4));
 }
 /*  Finished.
@@ -163,7 +291,6 @@ void NCursesDisplay::DisplayData(WINDOW *window, std::vector<std::vector<std::st
   mvwprintw(window, ++row_data, column_data, column_names.c_str());
   wattroff(window, COLOR_PAIR(7));
 
-
   // Start assuming there are no data holes
   for (int i = 0; i < plotData.size(); i++) {
     std::string time_str = timeStampToHReadble(stol(plotData[i][0]) / 1000);
@@ -171,7 +298,7 @@ void NCursesDisplay::DisplayData(WINDOW *window, std::vector<std::vector<std::st
     wattron(window, COLOR_PAIR(4));
     mvwprintw(window, ++row_data, column_data, dataString.c_str());
     wattroff(window, COLOR_PAIR(4));
-    if (i > 39){
+    if (i > window->_maxx){
       break; 
     }
   }
@@ -200,10 +327,17 @@ void NCursesDisplay::Display(int n) {
 	view.xmin = XMIN; view.xmax = XMAX;
 	view.ymin = YMIN; view.ymax = YMAX;
 	view.xscl = XSCL; view.yscl = YSCL;
+  view.current_coin = COIN_TO_PLOT; 
 
   while (1) {
     // Be able to change agent selection -> right hand side window
     // Displays agents calculated AStar path
+    switch(wgetch(system_window)){
+      case 'w':
+      editViewWindow(&view);
+      break;
+    }
+
     // switch (wgetch(system_window)) {
     // case KEY_F0 + 1:
     //   agentNumber += (agentNumber < (maxAgentNumber - 1)) ? 1 : -agentNumber;
@@ -212,6 +346,8 @@ void NCursesDisplay::Display(int n) {
     //   agentNumber -= (agentNumber > 0) ? 1 : -(maxAgentNumber - 1);
     //   break;
     // }
+    // int key = getch();
+    // if (key == 'w') editViewWindow(&view);
     std::vector<std::vector<std::string>> plotData = orchestrator.getPlotData();
 
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
@@ -221,7 +357,8 @@ void NCursesDisplay::Display(int n) {
     init_pair(5, COLOR_CYAN, COLOR_BLACK);
     init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(7, COLOR_GREEN, COLOR_WHITE); 
-    init_pair(8, COLOR_WHITE, COLOR_GREEN); 
+    init_pair(8, COLOR_WHITE, COLOR_GREEN);
+    init_pair(9, COLOR_WHITE, COLOR_BLACK); 
     wclear(system_window); 
     wclear(plot_window);
     wclear(data_window); 
