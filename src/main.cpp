81aae85a7763a45f3acc38acae215042dec05d54
edghyhdz <edgar.hdz@hotmail.com>
2021-02-15 07:05:08 +0100

    Minor update g++

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
 
 g++ main.cpp Agent.h Agent.cpp WaitingArea.h WaitingArea.cpp ncurses_display.h ncurses_display.cpp -lcurses -pthread -o display.o
-g++ -g src/QueryCrypto.cpp src/QueryCrypto.h src/Binance.h src/Orchestrator.cpp src/Orchestrator.h src/ncurses_display.h src/ncurses_display.cpp src/main.cpp -o run.test -lcurses -lcurl -pthread
+g++ -g src/QueryCrypto.cpp src/QueryCrypto.h src/Binance.h src/Orchestrator.cpp src/Orchestrator.h src/ncurses_display.h src/ncurses_display.cpp src/main.cpp -o run.test -lcurses -lcurl -pthread -lform
 */
 
 #include "ncurses_display.h"
