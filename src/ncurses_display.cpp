26030d49225b366391fdf7f36167450d05614531
edghyhdz <edgar.hdz@hotmail.com>
2021-02-16 13:23:39 +0100

    Added extra field in editViewWindow, added drawSubPlot to plot multiple coins at the same time

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -136,12 +136,13 @@ int NCursesDisplay::editViewWindow(viewwin *view)
 	mvwprintw(fwin, 0, 4, " VIEW WINDOW ");
 	mvwprintw(fwin, 2, 3, "COIN 1 =");
 	mvwprintw(fwin, 3, 3, "COIN 2 =");
+	mvwprintw(fwin, 4, 3, "COIN 2 =");
 
 	wattroff(fwin, A_BOLD);
 	mvwprintw(fwin, 9, 6, "[SPC] OK");
 	/* Create the form fields */
-	FIELD *fields[3];
-	int i; for (i=0; i<2; i++) {
+	FIELD *fields[4];
+	int i; for (i=0; i<3; i++) {
 		fields[i] = new_field(1, 8, i, 0, 0, 0);
 		set_field_back(fields[i], A_REVERSE | A_UNDERLINE);
 		set_field_type(fields[i], TYPE_ALNUM, 6, 0.0, 0.0);
@@ -149,11 +150,13 @@ int NCursesDisplay::editViewWindow(viewwin *view)
 		field_opts_off(fields[i], O_AUTOSKIP | O_STATIC);
 		set_max_field(fields[i], FIELD_MAX_CHARS);
 	}
-	fields[2] = NULL;
+	fields[3] = NULL;
 
 	/* Fill the form fields with initial values */
 	char printbuf[FIELD_MAX_CHARS+1];
-	set_field_buffer(fields[0], 0, (view->current_coin).c_str());
+	set_field_buffer(fields[0], 0, (view->first_coin).c_str());
+	set_field_buffer(fields[1], 0, (view->secondary_coin).c_str());
+	set_field_buffer(fields[2], 0, (view->third_coin).c_str());
 
 	/* Create a subwindow for the form fields */
 	WINDOW *fsub = derwin(fwin, 6, 8, 2, 10);
@@ -197,8 +200,10 @@ int NCursesDisplay::editViewWindow(viewwin *view)
 
 	if (savewin) {
           form_driver(f, REQ_VALIDATION);
-          view->current_coin = field_buffer(fields[0], 0);
-	}
+          view->first_coin = field_buffer(fields[0], 0);
+          view->secondary_coin = field_buffer(fields[1], 0);
+          view->third_coin = field_buffer(fields[2], 0);
+        }
 
   /* Clean up */
 	curs_set(0);
@@ -212,59 +217,77 @@ int NCursesDisplay::editViewWindow(viewwin *view)
 	return savewin;
 }
 
-void NCursesDisplay::DrawGraph(WINDOW *window, const viewwin *view, std::vector<std::vector<std::string>> &plotData) {
+void NCursesDisplay::DrawSubPlot(WINDOW *window, std::vector<std::vector<std::string>> &plotData, char ch, int color, std::string coin_name, int x_offset){
+  int xm, ym;
+  getmaxyx(window, ym, xm);
+  double max_p_range, min_p_range, max_ts_range, min_ts_range; 
+  double max_price, min_price; 
+  std::vector<double> price_data;
+  std::vector<double> time_stamps; 
+
+  // Remove any trailing spaces from coin_name
+  coin_name.erase(remove(coin_name.begin(), coin_name.end(), ' '), coin_name.end());
+  
+  if (plotData.size()>0){
+    if (plotData.size() > 0) {
+      for (int i = 0; i < plotData.size(); i++) {
+        price_data.push_back(stod(plotData[i][1]));
+        time_stamps.push_back(stol(plotData[i][0])); 
+      }
+      getMaxMinValue(price_data, &max_p_range, &min_p_range, PCTY); 
+      getMaxMinValue(time_stamps, &max_ts_range, &min_ts_range, PCTX); 
+      max_price = *std::max_element(price_data.begin(), price_data.end());
+      min_price = *std::min_element(price_data.begin(), price_data.end());
+    }
+
+    wattron(window, COLOR_PAIR(color));
+    std::reverse(plotData.begin(), plotData.end());
+    double last_price; 
+    for (int i = 0; i < plotData.size(); i++) {
+      double xpd, ypd;
+      xpd = scale(stod(plotData[i][0]), min_ts_range, max_ts_range, 0, xm);
+      ypd = scale(stod(plotData[i][1]), min_p_range, max_p_range, ym, 0);
+
+      xpd = (int)(xpd + 0.5 - (xpd < 0));
+      ypd = (int)(ypd + 0.5 - (ypd < 0));
+      mvwaddch(window, ypd, xpd, ch);
+
+      // Retrieve current price
+      if (i == plotData.size() - 1){
+        last_price = stod(plotData[i][1]); 
+      }
+    }
+
+    wattroff(window, COLOR_PAIR(color));
+    wattron(window, COLOR_PAIR(color));
+    wattron(window, A_BOLD);
+    mvwprintw(window, 1, xm - x_offset, ("COIN          : " + coin_name).c_str());
+    mvwprintw(window, 2, xm - x_offset, ("Current price : " + to_string(last_price) + " USDT").c_str());
+    wattroff(window, A_BOLD);
+    mvwprintw(window, 3, xm - x_offset, ("Max price     : " + to_string(max_price) + " USDT").c_str());
+    mvwprintw(window, 4, xm - x_offset, ("Min price     : " + to_string(min_price) + " USDT").c_str());
+    wattroff(window, COLOR_PAIR(color));
+  }
+}
+
+void NCursesDisplay::DrawGraph(WINDOW *window, const viewwin *view, std::vector<std::vector<std::vector<std::string>>> &allPlotData) {
   /* Draws a graph on the screen without axes.
      win              - ncurses window for drawing
      view             - view parameters structure
      yfunc            - function to graph (function pointer)
      enableSlopeChars - whether or not to call slopeChar to determine characters
   */
-  int xm, ym;
-  getmaxyx(window, ym, xm);
-  char ch = 'o';
-  double max_p_range, min_p_range, max_ts_range, min_ts_range; 
-  double max_price, min_price; 
-  std::vector<double> price_data;
-  std::vector<double> time_stamps; 
-  if (plotData.size() > 0) {
-    for (int i = 0; i < plotData.size(); i++) {
-      price_data.push_back(stod(plotData[i][1]));
-      time_stamps.push_back(stol(plotData[i][0])); 
-    }
-    getMaxMinValue(price_data, &max_p_range, &min_p_range, PCTY); 
-    getMaxMinValue(time_stamps, &max_ts_range, &min_ts_range, PCTX); 
-    max_price = *std::max_element(price_data.begin(), price_data.end());
-    min_price = *std::min_element(price_data.begin(), price_data.end());
+
+  // Vector size is limited to the amount of available coins as indicated in [_viewwin]
+  if (allPlotData[0].size() > 0) {
+    DrawSubPlot(window, allPlotData[0], 'o', 4, view->first_coin, 35);
   }
-
-
-  wattron(window, COLOR_PAIR(4));
-  std::reverse(plotData.begin(), plotData.end());
-  double last_price; 
-  for (int i = 0; i < plotData.size(); i++) {
-    double xpd, ypd;
-    xpd = scale(stod(plotData[i][0]), min_ts_range, max_ts_range, 0, xm);
-    ypd = scale(stod(plotData[i][1]), min_p_range, max_p_range, ym, 0);
-
-    xpd = (int)(xpd + 0.5 - (xpd < 0));
-    ypd = (int)(ypd + 0.5 - (ypd < 0));
-    mvwaddch(window, ypd, xpd, ch);
-
-    // Retrieve current price
-    if (i == plotData.size() - 1){
-      last_price = stod(plotData[i][1]); 
-    }
+  if (allPlotData[1].size() > 0) {
+    DrawSubPlot(window, allPlotData[1], '*', 5, view->secondary_coin, 70);
   }
-
-  wattroff(window, COLOR_PAIR(4));
-  wattron(window, COLOR_PAIR(4));
-  wattron(window, A_BOLD);
-  mvwprintw(window, 1, xm - 35, ("COIN          : " + view->current_coin).c_str());
-  mvwprintw(window, 2, xm - 35, ("Current price : " + to_string(last_price) + " USDT").c_str());
-  wattroff(window, A_BOLD);
-  mvwprintw(window, 3, xm - 35, ("Max price     : " + to_string(max_price) + " USDT").c_str());
-  mvwprintw(window, 4, xm - 35, ("Min price     : " + to_string(min_price) + " USDT").c_str());
-  wattroff(window, COLOR_PAIR(4));
+  if (allPlotData[2].size() > 0) {
+    DrawSubPlot(window, allPlotData[2], '+', 6, view->third_coin, 105);
+  }
 }
 /*  Finished.
     flarn2006's cool repo
@@ -318,11 +341,18 @@ void NCursesDisplay::DisplayHTTPStats(WINDOW *window, int requestWeight) {
   wattron(window, A_BOLD);
   wattron(window, COLOR_PAIR(4));
   std::string title = "Current request weight:";
-  mvwprintw(window, 3, window->_maxx - 25, title.c_str());
-  mvwprintw(window, 4, window->_maxx - 25, (to_string(requestWeight) + "/1200/min").c_str()); 
+  mvwprintw(window, 1, window->_maxx - 25, title.c_str());
+  mvwprintw(window, 2, window->_maxx - 25, (to_string(requestWeight) + "/1200/min").c_str()); 
   wattroff(window, COLOR_PAIR(4));
   wattroff(window, A_BOLD);
 
+}
+
+void setCoinsVector(std::vector<std::string> *coins_vector, viewwin &view){
+  (*coins_vector).clear(); 
+  (*coins_vector).push_back(view.first_coin);
+  (*coins_vector).push_back(view.secondary_coin);
+  (*coins_vector).push_back(view.third_coin);
 }
 
 void NCursesDisplay::Display(int n) {
@@ -339,7 +369,9 @@ void NCursesDisplay::Display(int n) {
   // Start http requests
   Orchestrator orchestrator = Orchestrator();
   orchestrator.runQuery();
-  orchestrator.setCoinToPlot(COIN_TO_PLOT);
+
+  // TODO: Remove -> deprecated
+  // orchestrator.setCoinToPlot(COIN_TO_PLOT);
   
   // Send keys timeout
   keypad(system_window, true);
@@ -349,19 +381,27 @@ void NCursesDisplay::Display(int n) {
 	view.xmin = XMIN; view.xmax = XMAX;
 	view.ymin = YMIN; view.ymax = YMAX;
 	view.xscl = XSCL; view.yscl = YSCL;
-  view.current_coin = COIN_TO_PLOT; 
+  view.first_coin = COIN_TO_PLOT; 
+  view.secondary_coin = COIN_TO_PLOT_SECOND; 
+  view.third_coin = ""; 
+  std::vector<std::string> coins_vector; 
+  std::string price; 
+
+  setCoinsVector(&coins_vector, view); 
+  orchestrator.setCoinsToPlot(coins_vector); 
 
   while (1) {
-    // Be able to change agent selection -> right hand side window
-    // Displays agents calculated AStar path
     switch(wgetch(system_window)){
-      case 'w':
+      case KEY_F0 + 1:
       editViewWindow(&view);
-      orchestrator.setCoinToPlot(view.current_coin);
+      // orchestrator.setCoinToPlot(view.first_coin);
+      setCoinsVector(&coins_vector, view); 
+      orchestrator.setCoinsToPlot(coins_vector); 
       break;
     }
     int requestWeight = orchestrator.getCurrentWeightRequest(); 
-    std::vector<std::vector<std::string>> plotData = orchestrator.getPlotData();
+    std::vector<std::vector<std::vector<std::string>>> allPlotData = orchestrator.getAllPlotData();
+
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_RED, COLOR_BLACK);
@@ -379,10 +419,29 @@ void NCursesDisplay::Display(int n) {
     box(plot_window, 0, 0);
     box(data_window, 0, 0);
     DisplayHTTPStats(system_window, requestWeight);
-    DisplayData(data_window, plotData); 
     DrawAxes(plot_window, &view);
-    DrawGraph(plot_window, &view, plotData); 
-    mvwprintw(system_window, 0, 0, orchestrator.getCoinToPlot().c_str());
+    if (allPlotData.size() > 0){ 
+      if (!allPlotData[0].empty()){
+        DisplayData(data_window, allPlotData[0]); 
+        DrawGraph(plot_window, &view, allPlotData);
+      }
+    }
+
+    int row_temp = 0;
+    for (std::string coin : orchestrator.getCoinsToPlot()) {
+      coin = coin + ":";
+      mvwprintw(system_window, ++row_temp, 1, coin.c_str());
+    }
+
+    row_temp = 0;
+    if (allPlotData.size() > 0){
+      for (int k=0; k<allPlotData.size(); k++){
+        if (!allPlotData[k].empty()){
+          price = allPlotData[k].front()[1];
+          mvwprintw(system_window, ++row_temp, 10, price.c_str());
+        }
+      }
+    }
     wrefresh(system_window);
     wrefresh(plot_window);
     wrefresh(data_window); 
@@ -390,12 +449,6 @@ void NCursesDisplay::Display(int n) {
 
     // Speed in which it is refreshed
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-
-    // for (int i = 0; i < plotData.size();  i++){
-    //   std::cout << plotData[i][0] << ":" << plotData[i][1]; 
-    // }
-
-
   }
   endwin();
 }
