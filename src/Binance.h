31cb2e27606e0ca40385924c4ed24f57299cd9f9
edghyhdz <edgar.hdz@hotmail.com>
2021-02-20 12:02:35 +0100

    Added keywords for parsing json response

--- src/Binance.h
+++ src/Binance.h
@@ -16,46 +16,15 @@ const std::string USED_WEIGHT_PER_INTERVAL{"x-mbx-used-weight-1m"};
 const std::string HTTP_RESPONSE{"HTTP/1.1 200 OK"};
 const std::string RESPONSE{"Response"};
 const std::string OK_RESPONSE_STR{"200"};
-const std::string BAD_RESPONSE{"404"}; 
+const std::string BAD_RESPONSE{"404"};
+const std::string FIRST_KEY_WORD{"makerCommission"}; // Used to parse json data
+const std::string LAST_KEY_WORD{"SPOT"}; // Used to parse json data
 const int INTERVAL_LIMIT(1100); 
 const int MAX_WORKER_SIZE(60); 
 const int MAX_WORKER_BATCH_SIZE(30); 
 const int MAX_COIN_TO_PLOT(4); 
 const int KEY_COUNTER(3);
 const long OK_RESPONSE(200);  
-
-// get current directory
-// reference https://www.daniweb.com/programming/software-development/threads/406729/current-directory
-std::string getCurrentDirectory(){
-  char strDir[129] = {0};
-  getcwd(strDir, 128);
-
-  return strDir; 
-}
-
-// Split
-// reference https://stackoverflow.com/a/14266139/13743493
-std::vector<std::string> split(const std::string &str, const std::string &delim) {
-  std::vector<std::string> tokens;
-  size_t prev = 0, pos = 0;
-  do {
-    pos = str.find(delim, prev);
-    if (pos == std::string::npos)
-      pos = str.length();
-    std::string token = str.substr(prev, pos - prev);
-    if (!token.empty())
-      tokens.push_back(token);
-    prev = pos + delim.length();
-  } while (pos < str.length() && prev < str.length());
-  return tokens;
-}
-
-// Callback to curl function
-// Reference https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
-size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp) {
-  ((std::string *)userp)->append((char *)contents, size * nmemb);
-  return size * nmemb;
-}
 
 } // namespace Binance
 
