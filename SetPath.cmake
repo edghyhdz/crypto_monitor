c56d8dc4cfd294b15574bd31583669587b054fb0
edghyhdz <edgar.hdz@hotmail.com>
2021-02-22 18:51:09 +0100

    Changes to .h file name

--- SetPath.cmake
+++ SetPath.cmake
@@ -1,2 +1,2 @@
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../src/DataPath.h.in ${CMAKE_CURRENT_SOURCE_DIR}/../src/DataPath.h)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../src/Variables.h.in ${CMAKE_CURRENT_SOURCE_DIR}/../src/Variables.h)
 message("Data path was set") # For testing only
